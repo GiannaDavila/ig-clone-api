cdce00b482a70b95c4dca8f12dad4f713aa2d2f0
GiannaDavila <giannagiannadavila@gmail.com>
2022-11-16 14:35:42 -0500

    first coomit

---
+++ functions/index.js
@@ -0,0 +1,14 @@
+import functions from 'firebase-functions'
+import express from 'express'
+import cors from 'cors'
+import { getAllPhotos, addNewPhotos } from './src/photo.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());//any time we do anything other than .get 
+
+//routes here ....
+app.get('/photos', getAllPhotos);  //get all photos
+app.post('/photos', addNewPhotos); //add new photos
+
+export const api = functions.https.onRequest(app);

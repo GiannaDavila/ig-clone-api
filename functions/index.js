ea8297061cb4a461357fac667644ea9fae79d37e
GiannaDavila <giannagiannadavila@gmail.com>
2022-11-17 20:26:17 -0500

    added like button

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 import functions from 'firebase-functions'
 import express from 'express'
 import cors from 'cors'
-import { getAllPhotos, addNewPhotos } from './src/photo.js';
+import { getAllPhotos, addNewPhotos, addLike } from './src/photo.js';
 
 const app = express();
 app.use(cors());
@@ -10,5 +10,6 @@ app.use(express.json());//any time we do anything other than .get
 //routes here ....
 app.get('/photos', getAllPhotos);  //get all photos
 app.post('/photos', addNewPhotos); //add new photos
+app.patch('/photos,:photoId', addLike)
 
 export const api = functions.https.onRequest(app);
--- functions/src/photo.js
+++ functions/src/photo.js
@@ -1,9 +1,10 @@
+import { FieldValue } from "firebase-admin/firestore";
 import dbConnect from "./dbConnect.js";
 export async function getAllPhotos (req,res){
     const db = dbConnect();
     const collection = await db.collection('photos') .get()
     .catch (err => res.status(500).send(err))
-    const photos = collection.docs.map(doc => ({...doc.data(), photoId: doc.id}));
+    const photos = collection.docs.map(doc => ({...doc.data(), photoId: doc.id}));//1.info in the data 3. the random number id name 2.name for this 
     res.send(photos);
 
 }
@@ -13,5 +14,14 @@ export async function addNewPhotos (req,res){
     await db.collection('photos').add(newPhoto)
     .catch(err => res.status(500).send(err));
     getAllPhotos(req,res); 
+}
+export async function addLike (req, res){
+    const {photoId} = req.params 
+    const db = dbConnect();
+    await db.collection('photos').doc(photoId)
+    .update({likes: FieldValue.increment(1)})
+    .catch(err => res.status(500).send(err));
+    getAllPhotos(req,res)
+
 }
 
